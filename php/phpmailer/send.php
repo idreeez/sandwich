c5b4bb1632340a91846233d8fe760a3703d82b30
idreeez <107794971+idreeez@users.noreply.github.com>
2025-11-25 10:52:12 +0300

    Add files via upload

---
+++ php/phpmailer/send.php
@@ -0,0 +1,76 @@
+<?php
+// Файлы phpmailer
+require 'PHPMailer.php';
+require 'SMTP.php';
+require 'Exception.php';
+
+# проверка, что ошибки нет
+if (!error_get_last()) {
+
+    // Переменные, которые отправляет пользователь
+    $name = $_POST['name'] ;
+    $tel = $_POST['tel'];
+    $text = $_POST['text'];
+    
+    // Формирование самого письма
+    $title = "Новая заявка";
+    $body = "
+    <h2>Новое письмо</h2>
+    <b>Имя:</b> $name<br>
+    <b>Телефон:</b> $tel<br><br>
+    <b>Сообщение:</b><br>$text
+    ";
+    
+    // Настройки PHPMailer
+    $mail = new PHPMailer\PHPMailer\PHPMailer();
+    
+    $mail->isSMTP();   
+    $mail->CharSet = "UTF-8";
+    $mail->SMTPAuth   = true;
+    $mail->SMTPDebug = 2;
+    $mail->Debugoutput = function($str, $level) {$GLOBALS['data']['debug'][] = $str;};
+    
+    // Настройки вашей почты
+    $mail->Host       = 'smtp.yandex.ru'; // SMTP сервера вашей почты
+    $mail->Username   = 'coffeetimekam'; // Логин на почте
+    $mail->Password   = 'vvszpgkutylzwzhf'; // Пароль на почте
+    $mail->SMTPSecure = 'ssl';
+    $mail->Port       = 465;
+    $mail->setFrom('coffeetimekam@yandex.ru', 'Name'); // Адрес самой почты и имя отправителя
+    
+    // Получатель письма
+    $mail->addAddress('coffeetimekam@yandex.ru');  
+    
+    // Прикрипление файлов к письму
+    if (!empty($file['name'][0])) {
+        for ($i = 0; $i < count($file['tmp_name']); $i++) {
+            if ($file['error'][$i] === 0) 
+                $mail->addAttachment($file['tmp_name'][$i], $file['name'][$i]);
+        }
+    }
+    // Отправка сообщения
+    $mail->isHTML(true);
+    $mail->Subject = $title;
+    $mail->Body = $body;    
+    
+    // Проверяем отправленность сообщения
+    if ($mail->send()) {
+        $data['result'] = "success";
+        $data['info'] = "Сообщение успешно отправлено!";
+    } else {
+        $data['result'] = "error";
+        $data['info'] = "Сообщение не было отправлено. Ошибка при отправке письма";
+        $data['desc'] = "Причина ошибки: {$mail->ErrorInfo}";
+    }
+    
+} else {
+    $data['result'] = "error";
+    $data['info'] = "В коде присутствует ошибка";
+    $data['desc'] = error_get_last();
+}
+
+// Отправка результата
+header('Content-Type: application/json');
+echo json_encode($data);
+
+?>
